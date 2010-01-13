f9567364628f6eb46396af4d7f7f16b2960071e0
Keiji, Yoshimi <walf443@gmail.com>
2010-01-14 00:51:45 +0900

    initial commit

---
+++ lib/DBIx/Skinny/Pager.pm
@@ -0,0 +1,33 @@
+package DBIx::Skinny::Pager;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Skinny::Pager -
+
+=head1 SYNOPSIS
+
+  use DBIx::Skinny::Pager;
+
+=head1 DESCRIPTION
+
+DBIx::Skinny::Pager is
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
