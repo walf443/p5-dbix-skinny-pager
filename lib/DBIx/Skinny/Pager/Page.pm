8b9f8a60d8a1e6cbffcf7c152733ee72055cb742
Keiji, Yoshimi <walf443@gmail.com>
2010-03-30 00:46:56 +0900

    added method to_hash to DBIx::Skinny::Pager::Page.
    It's useful for output pager with JSONRPC.

--- lib/DBIx/Skinny/Pager/Page.pm
+++ lib/DBIx/Skinny/Pager/Page.pm
@@ -3,6 +3,17 @@ use strict;
 use warnings;
 use base qw(Data::Page);
 
+sub to_hash {
+    my $self = $_[0];
+    return +{
+        total_entries    => $self->total_entries,
+        entries_per_page => $self->entries_per_page,
+        previous_page    => $self->previous_page,
+        current_page  => $self->current_page,
+        next_page        => $self->next_page,
+    };
+}
+
 1;
 
 __END__
