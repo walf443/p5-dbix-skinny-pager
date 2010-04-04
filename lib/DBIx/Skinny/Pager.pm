90c86bd5d9339ac4dc13e0b95aedf9fd909f1779
Keiji, Yoshimi <walf443@gmail.com>
2010-04-05 07:16:53 +0900

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 528adf3..c839ae4 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,7 @@
     Revision history for Perl extension DBIx::Skinny::Pager
    
    +0.03    Mon Apr  5 07:15:48 JST 2010
    +        - added Test::Deep to build_requires.
     0.02    Mon Apr  5 01:48:40 JST 2010
             - added DBIx::Skinny::Pager::Page to_hash method.
             - DBIx::Skinny::Pager return DBIx::Skinny::Pager::ResultSet object if not wantarry.

--- lib/DBIx/Skinny/Pager.pm
+++ lib/DBIx/Skinny/Pager.pm
@@ -6,7 +6,7 @@ use base 'DBIx::Skinny::SQL';
 use DBIx::Skinny::Pager::Page;
 use DBIx::Skinny::Pager::ResultSet;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 __PACKAGE__->mk_accessors(qw(page));
 
